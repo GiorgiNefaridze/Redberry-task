bd7702cb42830669cc35ecf9f934fff41acf9bf6
Giorgi Nefaridze <nefogio1@gmail.com>
2022-08-27 22:43:35 -0700

    implement some validation on submit event

--- src/components/EmployeeInformation/EmployeeInformation.jsx
+++ src/components/EmployeeInformation/EmployeeInformation.jsx
@@ -1,6 +1,92 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import GoBack from "../../images/go-back.png";
+
 import "./EmployeeInformation.scss";
 
+//validation
+const redberryMailValidation = "redberry.ge";
+const georgianPhoneNumCode = "995";
+
+const georgianLetters = [
+  "ა",
+  "ბ",
+  "გ",
+  "დ",
+  "ე",
+  "ვ",
+  "ზ",
+  "თ",
+  "ი",
+  "კ",
+  "ლ",
+  "მ",
+  "ნ",
+  "ო",
+  "პ",
+  "ჟ",
+  "რ",
+  "ს",
+  "ტ",
+  "უ",
+  "ფ",
+  "ქ",
+  "ღ",
+  "ყ",
+  "შ",
+  "ჩ",
+  "ც",
+  "ძ",
+  "წ",
+  "ჭ",
+  "ხ",
+  "ჯ",
+  "ჰ",
+];
+
 export default function EmployeeInformation() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [direction, setDirection] = useState("");
+  const [position, setPosition] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    if (
+      name === "" ||
+      name.length < 2 ||
+      lastName === "" ||
+      lastName.length < 2 ||
+      email === "" ||
+      number === ""
+    )
+      return;
+
+    for (let i = 0; i < georgianLetters.length; i++) {
+      for (let j = 0; j < name.length; j++) {
+        if (georgianLetters[i] === name[j]) {
+          return true;
+        }
+      }
+    }
+
+    if (email.split("@")[1] !== redberryMailValidation) return;
+
+    if (!number.startsWith(georgianPhoneNumCode)) {
+      return;
+    }
+  };
+
   return (
     <div className="EmployeeInformation">
       <div className="EmployeeInformation-toggle-section">
@@ -8,21 +94,39 @@ export default function EmployeeInformation() {
         <span>ლეპტოპის მახასიათებლები</span>
       </div>
       <div className="EmployeeInformation-content">
-        <div className="EmployeeInformation-content-inner">
+        <form
+          onSubmit={(e) => submitForm(e)}
+          className="EmployeeInformation-content-inner"
+        >
           <div className="EmployeeInformation-content-inner-firstName-lastName">
             <div className="EmployeeInformation-content-inner-firstName-lastName-firstName">
               <label htmlFor="firstName">სახელი</label>
-              <input id="firstName" type="text" placeholder="სახელი" />
-              <span>ვალიდაცია</span>
+              <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                id="firstName"
+                type="text"
+                placeholder="სახელი"
+              />
+              <span>მინიმუმ 2 სიმბოლო, ქართული ასოები</span>
             </div>
             <div className="EmployeeInformation-content-inner-firstName-lastName-lastName">
               <label htmlFor="lastName">გვარი</label>
-              <input id="lastName" type="text" placeholder="გვარი" />
-              <span>ვალიდაცია</span>
+              <input
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                id="lastName"
+                type="text"
+                placeholder="გვარი"
+              />
+              <span>მინიმუმ 2 სიმბოლო, ქართული ასოები</span>
             </div>
           </div>
           <div className="EmployeeInformation-content-inner-team">
-            <select>
+            <select
+              value={direction}
+              onChange={(e) => setDirection(e.target.value)}
+            >
               <option value="თიმი">თიმი</option>
               <option value="დეველოპმენტი">დეველოპმენტი</option>
               <option value="HR">HR</option>
@@ -32,7 +136,10 @@ export default function EmployeeInformation() {
             </select>
           </div>
           <div className="EmployeeInformation-content-inner-position">
-            <select>
+            <select
+              value={position}
+              onChange={(e) => setPosition(e.target.value)}
+            >
               <option value="პოზიცია">პოზიცია</option>
             </select>
           </div>
@@ -40,6 +147,8 @@ export default function EmployeeInformation() {
             <label htmlFor="mail">მეილი</label>
             <input
               id="mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               type="email"
               placeholder="yourmail@redberry.ge"
               required
@@ -48,14 +157,26 @@ export default function EmployeeInformation() {
           </div>
           <div className="EmployeeInformation-content-inner-phone-number">
             <label htmlFor="number">ტელეფონის ნომერი</label>
-            <input id="number" type="text" placeholder="+995 598 00 07 01" />
+            <input
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+              id="number"
+              type="number"
+              placeholder="+995 598 00 07 01"
+            />
             <span>უნდა აკმაყოფილებდეს ქართული მობ-ნომრის ფორმატს</span>
           </div>
           <div className="EmployeeInformation-content-inner-button">
             <button type="submit">შემდეგი</button>
           </div>
-        </div>
+        </form>
       </div>
+      <img
+        onClick={() => goBack()}
+        className="EmployeeInformation-go-back"
+        title="უკან დაბრუნება"
+        src={GoBack}
+      />
     </div>
   );
 }
